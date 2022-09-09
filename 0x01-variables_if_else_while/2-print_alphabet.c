e86d405006dc6e6df28678907e75d4ccd9fa4e77
Natucoder <hailemichaelnat30@gmail.com>
2022-09-09 11:44:15 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char N;
+for (N = 'a'; N <= 'z'; N++)
+{
+putchar(N);
+}
+putchar('\n');
+return (0);
+}
