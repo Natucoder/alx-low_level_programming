7a1f0788cb68f0d08be11e675a520d18922636a1
Natucoder <hailemichaelnat30@gmail.com>
2022-09-09 11:53:12 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char N;
+for (N = '0'; N <= '9'; N++)
+{
+putchar(N);
+}
+putchar('\n');
+return (0);
+}
